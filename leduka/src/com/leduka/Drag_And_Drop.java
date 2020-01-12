28156f5357c3f22e45a11a8c3083aaf34af1512b
HP <HP@DESKTOP-AM0Q8B9>
2020-01-12 21:24:50 +0530

    added spicejet

--- leduka/src/com/leduka/Drag_And_Drop.java
+++ leduka/src/com/leduka/Drag_And_Drop.java
@@ -13,10 +13,10 @@ import org.openqa.selenium.interactions.Actions;
 public class Drag_And_Drop {
 	
 	public static void main(String[] args) throws InterruptedException {
-		//System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromelatest\\chrome79\\chromedriver.exe");
-		//WebDriver driver = new ChromeDriver();
-		System.setProperty("webdriver.ie.driver","E:\\Selenium\\IEDriverServer");
-		WebDriver driver=new InternetExplorerDriver();
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromelatest\\chrome79\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+//		System.setProperty("webdriver.ie.driver","E:\\Selenium\\IEDriverServer");
+//		WebDriver driver=new InternetExplorerDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://jqueryui.com/droppable/");
